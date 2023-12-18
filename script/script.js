caf8b57c068ac2232e3b958b9fa1f86874cfff59
Theorignajjjaaa <59120862+Theorignajjjaaa@users.noreply.github.com>
2023-12-18 18:54:26 +0530

    function 1.0

--- script/script.js
+++ script/script.js
@@ -1,10 +1,19 @@
+let dsi = document.querySelector('.tick')
+
+dsi.onclick = () =>{
+    alert("clears")
+    let contents = document.querySelector('.read').innerHTML;
+    contents = ("");
+}
+
 const downloadFile = () => {
-    alert("file Downloaded")
+    alert("file Downloaded");
     const link = document.createElement("a");
-    const content = document.querySelector('.read').textContent;
+    let content = document.querySelector('.read').textContent.replace(/ /g, "\n");
     const file = new Blob([content],{ type: 'text, charset=utf-8'});
     link.href = URL.createObjectURL(file);
     link.download = "sample.txt";
     link.click();
     URL.revokeObjectURL(link.href);
- };
+};
+
