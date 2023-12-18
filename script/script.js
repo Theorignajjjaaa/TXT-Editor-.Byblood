1e41fa238a03bce81357a08b464d827bbe7e6ecf
Theorignajjjaaa <59120862+Theorignajjjaaa@users.noreply.github.com>
2023-12-18 19:51:38 +0530

    funtion 1.2
    now save function is working for local
    save

--- script/script.js
+++ script/script.js
@@ -2,15 +2,16 @@ let dsi = document.querySelector('.tick')
 
 dsi.onclick = () =>{
     alert("clears")
-    let contents = document.querySelector('.read').innerHTML;
+    let contents = document.querySelector('.read').innerText;
     contents = ("");
 }
 
 const downloadFile = () => {
     alert("file Downloaded");
     const link = document.createElement("a");
-    let content = document.querySelector('.read').textContent.replace(/ /g, "\n");
-    const file = new Blob([content],{ type: 'text, charset=utf-8'});
+    let content = document.querySelector('.read').innerText;
+    const file = new Blob([content],{ type: 'text, charset=utf-8' , endings: "native"});
+    console.log(file)
     link.href = URL.createObjectURL(file);
     link.download = "sample.txt";
     link.click();
