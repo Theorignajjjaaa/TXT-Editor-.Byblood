366e021e9c281c38f433c232335799590262f6f8
Theorignajjjaaa <59120862+Theorignajjjaaa@users.noreply.github.com>
2023-12-18 20:54:13 +0530

    function 1.1
    just some quality's changes

--- script/script.js
+++ script/script.js
@@ -1,19 +1,32 @@
 let dsi = document.querySelector('.tick')
 
 dsi.onclick = () =>{
-    alert("clears")
-    let contents = document.querySelector('.read').innerText;
-    contents = ("");
+    if (confirm("Save ?")) {
+        downloadFile();
+      } else {
+        alert("Screen cleared!")
+        let contents = document.querySelector('.read');
+        contents.innerText = ("");
+      }
+    
 }
 
 const downloadFile = () => {
-    alert("file Downloaded");
+    let person = prompt("Please enter your name:", "Harry Potter");
+    if (person == "") {
+        person ="no-name"
+    } else if (person == null) {
+        return;
+    }
+    
+    
+    alert("File  Title( "+person+ " ) Downloaded");
     const link = document.createElement("a");
     let content = document.querySelector('.read').innerText;
     const file = new Blob([content],{ type: 'text, charset=utf-8' , endings: "native"});
     console.log(file)
     link.href = URL.createObjectURL(file);
-    link.download = "sample.txt";
+    link.download = person+".txt";
     link.click();
     URL.revokeObjectURL(link.href);
 };
